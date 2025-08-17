4b30040ae880547702c8cba15cdbb2ef6429d682
hodiencong <hodiencong2000@gmail.com>
2025-08-17 15:46:50 +0700

    fix build

--- components/ReactQueryProvider/index.tsx
+++ components/ReactQueryProvider/index.tsx
@@ -1,8 +1,28 @@
+import { useOnlineManager } from '@/hooks/useOnlineManager'
 import {
+  focusManager,
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
+import { AppState, AppStateStatus, Platform } from 'react-native'
+
+function onAppStateChange(status: AppStateStatus) {
+  // React Query already supports in web browser refetch on window focus by default
+  if (Platform.OS !== 'web') {
+    focusManager.setFocused(status === 'active')
+  }
+}
+
+
+function useAppState(onChange: (status: AppStateStatus) => void) {
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onChange)
+    return () => {
+      subscription.remove()
+    }
+  }, [onChange])
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,6 +33,10 @@ const queryClient = new QueryClient({
 })
 
 const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
+  useOnlineManager()
+
+  useAppState(onAppStateChange)
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
